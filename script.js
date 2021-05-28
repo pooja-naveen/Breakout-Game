6930ea2db85241ddeaf68b14eb46288f16733f77
pooja-naveen <pooja.naveen9119@gmail.com>
2021-05-28 22:31:19 +0530

    paddle move added

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 let score = 0;
 
+const brickRowCount = 9;
+const brickColumnCount = 5;
+
 //create ball properties
 const ball = {
   x: canvas.width / 2,
@@ -25,6 +28,27 @@ const paddle = {
   dx: 0,
 };
 
+//create brick props
+const brickInfo = {
+  w: 70,
+  h: 20,
+  padding: 10,
+  offsetX: 45,
+  offsetY: 60,
+  visible: true,
+};
+
+//create bricks
+const bricks = [];
+for (let i = 0; i < brickRowCount; i++) {
+  bricks[i] = [];
+  for (let j = 0; j < brickColumnCount; j++) {
+    const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+    const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+    bricks[i][j] = { x, y, ...brickInfo };
+  }
+}
+
 //draw ball on canvas
 function drawBall() {
   ctx.beginPath();
@@ -43,20 +67,85 @@ function drawPaddle() {
   ctx.closePath();
 }
 
-//draw everything
-function draw() {
-  drawBall();
-  drawPaddle();
-  drawScore();
-}
-
 //draw score on canvas
 function drawScore() {
   ctx.font = "20px Arial";
   ctx.fillText(`Score:${score}`, canvas.width - 100, 30);
 }
 
-draw();
+//draw bricks on canvas
+function drawBricks() {
+  bricks.forEach((column) => {
+    column.forEach((brick) => {
+      ctx.beginPath();
+      ctx.rect(brick.x, brick.y, brick.w, brick.h);
+      ctx.fillStyle = brick.visible ? "#0095dd" : "transparent";
+      ctx.fill();
+      ctx.closePath();
+    });
+  });
+}
+//console.log(bricks);
+
+//move paddle on canvas
+function movePaddle() {
+  paddle.x += paddle.dx;
+
+  //wall detection
+  if (paddle.x + paddle.w > canvas.width) {
+    paddle.x = canvas.width - paddle.w;
+  }
+  if (paddle.x < 0) {
+    paddle.x = 0;
+  }
+}
+
+//draw everything
+function draw() {
+  //clear canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  drawPaddle();
+  drawScore();
+  drawBricks();
+}
+
+//update canvas drawing and animation
+function update() {
+  movePaddle();
+
+  //Draw everything
+  draw();
+
+  requestAnimationFrame(update);
+}
+update();
+
+//keydown event
+function keyDown(e) {
+  //   console.log(e.key);
+  if (e.key === "Right" || e.key === "ArrowRight") {
+    paddle.dx = paddle.speed;
+  } else if (e.key === "Left" || e.key === "ArrowLeft") {
+    paddle.dx = -paddle.speed;
+  }
+}
+
+//keyup event
+function keyUp(e) {
+  if (
+    e.key === "Right" ||
+    e.key === "ArrowRight" ||
+    e.key === "Left" ||
+    e.key === "ArrowLeft"
+  ) {
+    paddle.dx = 0;
+  }
+}
+
+//keyboard event handlers
+document.addEventListener("keydown", keyDown);
+document.addEventListener("keyup", keyUp);
 
 //rules and close event handlers
 rulesBtn.addEventListener("click", () => rules.classList.add("show"));
